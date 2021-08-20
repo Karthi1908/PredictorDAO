288e0b43aef724413c61d34cd54aca7679697731
Karthik <33004854+Karthi1908@users.noreply.github.com>
2021-08-20 16:10:22 +0800

    Add files via upload

---
+++ DAO/src/App.js
@@ -0,0 +1,26 @@
+import Home from './components/Home.js';
+import Navbar from './components/Navbar.js';
+import User from './components/User.js';
+import Title from './components/Title.js';
+import Details from './components/Details.js';
+import DaoDaoDetails from './components/DaoDaoDetails.js';
+
+function App() {
+  return (
+    <>
+      
+      <Title />
+      <Details />
+      <DaoDaoDetails />
+       <Home />
+       
+    
+       <Navbar />
+       
+        <User />
+     
+    </>
+  );
+}
+
+export default App;
